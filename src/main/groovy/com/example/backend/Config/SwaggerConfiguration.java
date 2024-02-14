e5267986d9483086254c063b6358484c39c9b4ce
seohyeon <uimi3606@gmail.com>
2024-02-15 00:54:53 +0900

    ADD Swagger 3.0 시도

--- src/main/groovy/com/example/backend/Config/SwaggerConfiguration.java
+++ src/main/groovy/com/example/backend/Config/SwaggerConfiguration.java
@@ -2,26 +2,51 @@ package com.example.backend.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Configuration
-@EnableSwagger2
+@EnableWebMvc
 public class SwaggerConfiguration {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.springboot.api"))
-                .paths(PathSelectors.any())
-                .build();
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder().title("IoT API")
+                .description("IoT API Docs").build();
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Spring Boot Open API Test with Swagger")
-                .description("설명 부분")
-                .version("1.0.0")
-                .build();
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .consumes(getConsumeContentTypes())
+                .produces(getProduceContentTypes())
+                .apiInfo(swaggerInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("com.nahwasa.iot.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false);
+    }
+
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
     }
 }
+
